e966299f8b9073e9f01826c60959761ecc11a102
luca <681992+lukka@users.noreply.github.com>
2020-11-13 01:39:53 +0100

    put the dump of env vars (output of 'vcpkg env') in a foldable output section

--- packages/run-cmake-lib/src/utils.ts
+++ packages/run-cmake-lib/src/utils.ts
@@ -12,53 +12,55 @@ export class CMakeUtils {
   }
 
   public async injectEnvVariables(vcpkgRoot: string, triplet: string, baseLib: baselib.BaseLib): Promise<void> {
-    if (!vcpkgRoot) {
-      vcpkgRoot = process.env[vcpkgGlobals.outVcpkgRootPath] ?? "";
+    await this.baseUtils.wrapOp(`Setup environment variables for triplet '${triplet}' using 'vcpkg env'`, async () => {
       if (!vcpkgRoot) {
-        throw new Error(`${vcpkgGlobals.outVcpkgRootPath} environment variable is not set.`);
+        vcpkgRoot = process.env[vcpkgGlobals.outVcpkgRootPath] ?? "";
+        if (!vcpkgRoot) {
+          throw new Error(`${vcpkgGlobals.outVcpkgRootPath} environment variable is not set.`);
+        }
       }
-    }
 
-    // Search for vcpkg tool and run it
-    let vcpkgPath: string = path.join(vcpkgRoot, 'vcpkg');
-    if (this.baseUtils.isWin32()) {
-      vcpkgPath += '.exe';
-    }
-    const vcpkg: baselib.ToolRunner = baseLib.tool(vcpkgPath);
-    vcpkg.arg("env");
-    vcpkg.arg("--bin");
-    vcpkg.arg("--include");
-    vcpkg.arg("--tools");
-    vcpkg.arg("--python");
-    vcpkg.line(`--triplet ${triplet} set`);
-
-    const options = {
-      cwd: vcpkgRoot,
-      failOnStdErr: false,
-      errStream: process.stdout,
-      outStream: process.stdout,
-      ignoreReturnCode: true,
-      silent: false,
-      windowsVerbatimArguments: false,
-      env: process.env
-    } as baselib.ExecOptions;
-
-    const output = await vcpkg.execSync(options);
-    if (output.code !== 0) {
-      throw new Error(`${output.stdout}\n\n${output.stderr}`);
-    }
-
-    const map = this.baseUtils.parseVcpkgEnvOutput(output.stdout);
-    for (const key in map) {
-      if (this.baseUtils.isVariableStrippingPath(key))
-        continue;
-      if (key.toUpperCase() === "PATH") {
-        process.env[key] = process.env[key] + path.delimiter + map[key];
-      } else {
-        process.env[key] = map[key];
+      // Search for vcpkg tool and run it
+      let vcpkgPath: string = path.join(vcpkgRoot, 'vcpkg');
+      if (this.baseUtils.isWin32()) {
+        vcpkgPath += '.exe';
       }
-      baseLib.debug(`set ${key}=${process.env[key]}`)
-    }
+      const vcpkg: baselib.ToolRunner = baseLib.tool(vcpkgPath);
+      vcpkg.arg("env");
+      vcpkg.arg("--bin");
+      vcpkg.arg("--include");
+      vcpkg.arg("--tools");
+      vcpkg.arg("--python");
+      vcpkg.line(`--triplet ${triplet} set`);
+
+      const options = {
+        cwd: vcpkgRoot,
+        failOnStdErr: false,
+        errStream: process.stdout,
+        outStream: process.stdout,
+        ignoreReturnCode: true,
+        silent: false,
+        windowsVerbatimArguments: false,
+        env: process.env
+      } as baselib.ExecOptions;
+
+      const output = await vcpkg.execSync(options);
+      if (output.code !== 0) {
+        throw new Error(`${output.stdout}\n\n${output.stderr}`);
+      }
+
+      const map = this.baseUtils.parseVcpkgEnvOutput(output.stdout);
+      for (const key in map) {
+        if (this.baseUtils.isVariableStrippingPath(key))
+          continue;
+        if (key.toUpperCase() === "PATH") {
+          process.env[key] = process.env[key] + path.delimiter + map[key];
+        } else {
+          process.env[key] = map[key];
+        }
+        baseLib.debug(`set ${key}=${process.env[key]}`)
+      }
+    });
   }
 
   public async injectVcpkgToolchain(args: string[], triplet: string, baseLib: baselib.BaseLib): Promise<string[]> {
@@ -84,7 +86,7 @@ export class CMakeUtils {
 
         // For Windows build agents, inject the environment variables used
         // for the MSVC compiler using the 'vcpkg env' command.
-        // This is not be needed for others compiler on Windows, but it should be harmless.
+        // This is not needed for others compiler on Windows, but it should be harmless.
         if (this.baseUtils.isWin32() && triplet) {
           if (triplet.indexOf("windows") !== -1) {
             process.env.CC = "cl.exe";
