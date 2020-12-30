d6cf7f37f5078369d704c9bd467aee4ea544ab4a
luca <681992+lukka@users.noreply.github.com>
2020-12-31 00:57:53 +0100

    drop useless code

--- packages/run-vcpkg-lib/src/utils.ts
+++ packages/run-vcpkg-lib/src/utils.ts
@@ -3,9 +3,6 @@
 // SPDX short identifier: MIT
 
 import * as path from 'path'
-import * as baselib from '@lukka/base-lib'
-
-const vcpkgAdditionalCachedPathsKey = "VCPKG_ADDITIONAL_CACHED_PATHS_KEY";
 
 export function getOrdinaryCachedPaths(vcpkgRootDir: string): string[] {
   const pathsToCache: string[] = [
