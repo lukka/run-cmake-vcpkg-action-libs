705403a7369a8604aa8d950872c5338cdc01dce4
luca <681992+lukka@users.noreply.github.com>
2022-12-23 18:25:09 -0800

    add vcpkg-format-string input

--- packages/run-cmake-lib/src/index.ts
+++ packages/run-cmake-lib/src/index.ts
@@ -1,2 +1,6 @@
+// Copyright (c) 2019-2020-2021-2022 Luca Cappa
+// Released under the term specified in file LICENSE.txt
+// SPDX short identifier: MIT
+
 export * from './cmake-globals';
 export * from './cmake-runner'
